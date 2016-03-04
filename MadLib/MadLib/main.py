c187cfc54e33833d7c3a0b866b4f5b23955339f4
Angel Valdes <AngelValdes@hotmail.com>
2016-03-03 23:58:52 -0500

    gather some initial information
    Declare variables and prompt the user for some basic info

--- MadLib/MadLib/main.py
+++ MadLib/MadLib/main.py
@@ -3,3 +3,7 @@ import datetime
 #Welcoming user
 print "Welcome to my story game!"
 
+#Gather some initial information
+name = raw_input("Enter your name: ")
+color = raw_input("Enter your favorite color: ")
+city = raw_input("Enter the city you live in: ")
