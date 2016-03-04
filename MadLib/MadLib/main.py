c828be6c8e9b4fc3293834cce2150510f74aeab2
Angel Valdes <AngelValdes@hotmail.com>
2016-03-03 23:59:53 -0500

    display message with basic info
    Use basic info and a template to compose a message

--- MadLib/MadLib/main.py
+++ MadLib/MadLib/main.py
@@ -7,3 +7,12 @@ print "Welcome to my story game!"
 name = raw_input("Enter your name: ")
 color = raw_input("Enter your favorite color: ")
 city = raw_input("Enter the city you live in: ")
+
+#Message template
+message1Template = '''
+   {name} you are doing great! but I wonder why would you like {color}, if you live in {city}?
+   '''
+#Replace place holders with values in message template
+message1Template = message1Template.format(**locals())
+#Print message
+print message1Template
