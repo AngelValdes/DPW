840862372e274aa954bc8df6e587abbb0e4c5b9c
Angel Valdes <AngelValdes@hotmail.com>
2016-03-09 22:25:26 -0500

    define head variable in Page class to contain common initial html elements

--- SimpleForm/SimpleForm/main.py
+++ SimpleForm/SimpleForm/main.py
@@ -13,7 +13,18 @@ class MainHandler(webapp2.RequestHandler):
 
 class Page(object): #class for page properties and behaviours
     def __init__(self): #initialization method
-        pass
+        #define head variable to contain common initial html elements
+        self.head = ''' 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Us</title>
+    <meta charset="utf-8" />
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <h2>Contact Us!</h2>
+'''
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
