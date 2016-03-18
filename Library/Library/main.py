7416603954a5bdea44e2821fccf02c73c1cac911
Angel Valdes <AngelValdes@hotmail.com>
2016-03-17 23:56:00 -0400

    import from external libraries

---
+++ Library/Library/main.py
@@ -0,0 +1,10 @@
+'''
+name: Angel Valdes
+date: 3/16/2016
+class: Design Patterns for Web Programming
+assignment: Library
+instructor: REBECCA CARROLL
+'''
+import webapp2 #use the webapp2 library
+from pages import FormPage, ResultPage #*
+from library import Car, CarInventory #*
