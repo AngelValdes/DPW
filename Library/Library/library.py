12cc73fb3213a0176dff26f641717fb9eb0adf4a
Angel Valdes <AngelValdes@hotmail.com>
2016-03-17 23:44:04 -0400

    setting up the library

---
+++ Library/Library/library.py
@@ -0,0 +1,9 @@
+'''
+name: Angel Valdes
+date: 3/16/2016
+class: Design Patterns for Web Programming
+assignment: Library
+instructor: REBECCA CARROLL
+'''
+#importing external library to use date time functionality
+import datetime
