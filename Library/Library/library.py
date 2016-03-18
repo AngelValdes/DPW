4c086b5c383cbf43603b7981a48b218f38f1edd2
Angel Valdes <AngelValdes@hotmail.com>
2016-03-17 23:44:51 -0400

    setting car object class

--- Library/Library/library.py
+++ Library/Library/library.py
@@ -7,3 +7,13 @@ instructor: REBECCA CARROLL
 '''
 #importing external library to use date time functionality
 import datetime
+
+#car object type class to define object attributes, behaviours, and business rules
+class Car(object):
+    def __init__(self):
+        #Initializing internal fields of object state
+        self.__brand_name =""
+        self.__year = 0
+        self.__price = 0
+
+    
