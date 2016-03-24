a1436e7f8eeeb5346e2331baeb7886dfed635d9e
Angel Valdes <AngelValdes@hotmail.com>
2016-03-24 17:41:05 -0400

    object data type initial definition

---
+++ Site/Site/data.py
@@ -0,0 +1,18 @@
+'''
+name: Angel Valdes
+date: 3/24/2016
+class: Design Patterns for Web Programming
+assignment: Dynamic Site
+instructor: REBECCA CARROLL
+'''
+# Data object to define the tool attributes
+class Tool(object):
+    def __init__(self):
+        self.price = ""
+        self.make = ""
+        self.model = ""
+        self.height = ""
+        self.width = ""
+        self.weight = ""
+        self.description = "" 
+
