e1bca866376e6687925c9d59bfb928d514af1577
Angel Valdes <AngelValdes@hotmail.com>
2016-03-24 17:54:57 -0400

    if key value pair in query string, then do something other wise display welcome page

--- Site/Site/main.py
+++ Site/Site/main.py
@@ -13,8 +13,26 @@ from library import Utility #use the extra utility class
 
 class MainHandler(webapp2.RequestHandler):    
     def get(self):# main function     
-       utility = Utility() #instantiate a utility object
-       page = ContentPage()  #instantiate a page object
+        utility = Utility() #instantiate a utility object
+        page = ContentPage()  #instantiate a page object
+        if self.request.GET: #determine if there is any query string variables
+           pass
+        else:
+            #show welcome page
+            page._body = """
+<h2>Welcome to my tools site</h2>      
+        <img src="images/airCompressor.jpg" />
+        <img src="images/drill.jpg" />
+        <img src="images/nailGun.jpg" />
+        <img src="images/sander.jpg" />
+        <img src="images/saw.jpg" />
+        <hr/>
+        <p>Please select one of the options from the <strong>menu above</strong> to get more information about any of my tools</p>
+"""
+        #use the utility extra class to display the date formatted
+        page._head = page._head.replace("{date}", utility.get_date())
+        #print page content
+        self.response.write(page.print_out())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
