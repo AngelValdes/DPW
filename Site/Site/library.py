d904009cb3bf6757b927157c9b0da57386ea56bc
Angel Valdes <AngelValdes@hotmail.com>
2016-03-24 17:57:00 -0400

    define library extra component

---
+++ Site/Site/library.py
@@ -0,0 +1,14 @@
+'''
+name: Angel Valdes
+date: 3/24/2016
+class: Design Patterns for Web Programming
+assignment: Dynamic Site
+instructor: REBECCA CARROLL
+'''
+#importing external library to use date time functionality
+import datetime
+
+class Utility(object): #utility library class
+    def __init__(self): #initialization method
+        pass
+ 
