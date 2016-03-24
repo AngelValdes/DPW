4a35becd38d60c1bddca62fe3a279c4c6770e42e
Angel Valdes <AngelValdes@hotmail.com>
2016-03-24 17:46:41 -0400

    initial page definition

---
+++ Site/Site/pages.py
@@ -0,0 +1,11 @@
+'''
+name: Angel Valdes
+date: 3/24/2016
+class: Design Patterns for Web Programming
+assignment: Dynamic Site
+instructor: REBECCA CARROLL
+'''
+
+class Page(object): #class with common functionality by all pages to be inherited
+    def __init__(self): #initialization method
+      pass
